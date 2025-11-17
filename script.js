715619fdae5da2f76088c558b093afb7d8ddd926
tothemoon07 <bcoronado000@gmail.com>
2025-11-17 13:07:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 // Inicializar el cliente de Supabase
-const supabase = createClient(
-  'https://ofyalhphejzwzizqkzmi.supabase.co', // Tu URL de Supabase
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meWFsaHBoZWp6d3ppenFrem1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzNTA4NzUsImV4cCI6MjA3ODkyNjg3NX0.RiTXrF_4qzLF27zaDnIAtKMBzyYrdMnzSzM0w1SBZF4' // Tu Key de Supabase
+const supabase = supabase.createClient(
+  'https://ofyalhphejzwzizqkzmi.supabase.co',  // URL de Supabase
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meWFsaHBoZWp6d3ppenFrem1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzNTA4NzUsImV4cCI6MjA3ODkyNjg3NX0.RiTXrF_4qzLF27zaDnIAtKMBzyYrdMnzSzM0w1SBZF4'  // Key de Supabase
 );
 
-// Función para obtener los sorteos de Supabase
+// Función para obtener los sorteos
 async function getSorteos() {
   const { data, error } = await supabase
-    .from('sorteos')  // Asegúrate de que el nombre de la tabla sea correcto
-    .select('*'); // Seleccionar todos los registros
-
+    .from('sorteos')
+    .select('*');
   if (error) {
     console.error(error);
     return null;
   }
-
   return data;
 }
 
 // Mostrar los sorteos
 window.onload = async function () {
   const sorteos = await getSorteos();
-
   if (sorteos) {
     const sorteosContainer = document.getElementById('sorteos-container');
     sorteos.forEach(sorteo => {
